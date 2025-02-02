b56d93580348443e7d1eb53fc6904a93a1b56dfb
Jefffer <jefre123@hotmail.com>
2025-02-02 14:17:02 +0100

    Experience component content added.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -76,6 +76,7 @@ const experiences = [
   {
     consultant: "Ditech Group",
     consultantLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnJWQ9jFRpq96bx2aVkT7rirbNJ9Uc-C6vXQ&s",
+    period: "January 2022 - Present",
     location: "Bilbao, Spain",
     clients: [
       {
@@ -100,12 +101,13 @@ const experiences = [
         role: "Senior .Net Developer",
         period: "May 2023 – Present",
         activities:
-          "Support in analysis and custom development of the AcceXible application using .NET 5 for Backend (Entity Framework) and Angular 12 for Frontend.",
+          "Custom developments (DPM) and version migrations of the CAPTOR system from .NET Framework 3.5 to 4.5 for client GESTAMP, using .NET for Backend, custom views (xui, xml) for Frontend, and SQL Server for relational database management.",
       },
     ],
   },
   {
     consultant: "Tata Consultancy Services",
+    period: "April 2021 - January 2022",
     consultantLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgdN5uaq0RMWIAydcXeyqdkm4dtErZs3sw0w&s",
     location: "Bogotá D.C",
     clients: [
@@ -116,6 +118,53 @@ const experiences = [
         period: "April 2021 – January 2022",
         activities:
           "Provided bilingual (Spanish and English) support for ticket resolution within the EMMA applications, managing .NET developments and SQL Server databases.",
+      },
+    ],
+  },
+  {
+    consultant: "Exsis Digital Angels",
+    period: "June 2017 - April 2021",
+    consultantLogo: "https://images.crunchbase.com/image/upload/c_pad,h_256,w_256,f_auto,q_auto:eco,dpr_1/et5zwmwaqsmtyao23keh",
+    location: "Bogotá D.C",
+    clients: [
+      {
+        name: "CHUBB",
+        logo: "https://1000marcas.net/wp-content/uploads/2021/10/Chubb-logo-tm-1280x720.png",
+        role: ".Net Developer",
+        period: "June 2019 - July 2020",
+        activities:
+          "Developed and supported functionalities in the Annualization module for Banco de Chile's insurance area using Clean Architecture in .NET Framework 4.5 and SQL Server (Entity Framework).",
+      },
+      {
+        period: "January 2018 - June 2019",
+        logo: "https://seeklogo.com/images/S/Seguros_del_Estado_S_A_-logo-1C31DF4CFE-seeklogo.com.png",
+        name: "Seguros del Estado (ICSG)",
+        role: "DBA, .Net Developer",
+        activities:
+          "Administered SQL Server databases, managed Windows Server and IIS, and supported/developed new modules for internal applications (Financing, Counter-guarantees, SARLAFT, etc.) using .NET Framework 3.5.",
+      },
+      {
+        period: "October 2017 - December 2017",
+        logo: "https://entelgy.com/wp-content/uploads/2015/07/ASA_COLPATRIA_Entelgy.png",
+        name: "AXA Colpatria",
+        role: "Junior .Net Developer",
+        activities:
+          "Performed functional tests for the Gemela system, and handled requirements analysis, design, and development of a complementary module for the SOAT insurance area using .NET Framework, SOAP services, and external APIs.",
+      },
+    ],
+  },
+  {
+    consultant: "Freelance",
+    consultantLogo: "https://scontent-mad2-1.xx.fbcdn.net/v/t39.30808-6/300788621_490409453088863_7310934356003262293_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=kIEXcsm83CoQ7kNvgERtno0&_nc_zt=23&_nc_ht=scontent-mad2-1.xx&_nc_gid=AjpMwJh6cZmBAO2XPXu1Wwx&oh=00_AYA1RrVRlWkZXOTSMGd-C6TRDz4gxsKYEJpM_I4N8XY_-w&oe=67A528BE",
+    location: "Bogotá D.C",
+    clients: [
+      {
+        name: "Viata Real State",
+        logo: "https://scontent-mad2-1.xx.fbcdn.net/v/t39.30808-6/300788621_490409453088863_7310934356003262293_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=kIEXcsm83CoQ7kNvgERtno0&_nc_zt=23&_nc_ht=scontent-mad2-1.xx&_nc_gid=AjpMwJh6cZmBAO2XPXu1Wwx&oh=00_AYA1RrVRlWkZXOTSMGd-C6TRDz4gxsKYEJpM_I4N8XY_-w&oe=67A528BE",
+        role: "Web Designer & Developer",
+        period: "May 2018 - September 2019",
+        activities:
+          "Designed, developed, implemented, and supported their website using PHP for backend, HTML & CSS for frontend, and phpMyAdmin on Linux hosting managed by cPanel.",
       },
     ],
   },
@@ -150,10 +199,10 @@ const Experience = () => {
                     className="w-16 h-16 object-contain"
                   />
                   <div>
-                    <h3 className="text-2xl font-bold text-gray-800">
+                    <h3 className="text-2xl font-bold text-gray-800 mt-0">
                       {exp.consultant}
                     </h3>
-                    <p className="text-gray-500 text-sm">{exp.location}</p>
+                    <p className="text-gray-500 text-sm mb-0">{exp.location}</p>
                   </div>
                 </div>
                 {expanded === index ? (
@@ -185,6 +234,7 @@ const Experience = () => {
                           {client.name}
                         </h4>
                         <p className="text-sm text-gray-500">{client.period}</p>
+                        <p className="text-m text-gray-500">{client.role}</p>
                         <p className="mt-2 text-gray-600 leading-relaxed">
                           {client.activities}
                         </p>
