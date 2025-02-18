3dfed54d7cf34b85ff540e169c5eecf316f3abb2
Jefffer <jefre123@hotmail.com>
2025-02-18 23:20:26 +0100

    Images uploaded in About component

--- src/components/NotAbout.jsx
+++ src/components/NotAbout.jsx
@@ -7,27 +7,32 @@ const skills = [
   {
     title: "Software Design & Backend Development",
     description: "Expertise in C#, .NET, Java, Python, PHP",
-    image: "https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?t=st=1739881200~exp=1739884800~hmac=0fe28d2d1d4c35b9dd50c6e829a665faf9ca86f82f7c8c4683e1e9ad0bd9ac9b&w=1060",
+    // image: "https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?t=st=1739881200~exp=1739884800~hmac=0fe28d2d1d4c35b9dd50c6e829a665faf9ca86f82f7c8c4683e1e9ad0bd9ac9b&w=1060",
+    image: "./software1.webp",
   },
   {
     title: "Frontend Development",
     description: "Building modern UIs with React, Angular, and more",
-    image: "https://img.freepik.com/free-vector/coding-concept-illustration_114360-22486.jpg?t=st=1739885189~exp=1739888789~hmac=3f96e1c0f88a6376d561206361922e8a9298cce144de8c0697ad4f05871914d4&w=826",
+    // image: "https://img.freepik.com/free-vector/coding-concept-illustration_114360-22486.jpg?t=st=1739885189~exp=1739888789~hmac=3f96e1c0f88a6376d561206361922e8a9298cce144de8c0697ad4f05871914d4&w=826",
+    image: "./database2.webp",
   },
   {
     title: "DevOps & Cloud",
     description: "Experience with Azure DevOps, AWS (ECS, EC2, S3)",
-    image: "https://img.freepik.com/free-vector/gradient-devops-illustration_23-2149370940.jpg?t=st=1739885266~exp=1739888866~hmac=6ff8276b323a2b8e5f04824f55bce1b26bdec002655407ea23071b48b17f78ca&w=1380",
+    // image: "https://img.freepik.com/free-vector/gradient-devops-illustration_23-2149370940.jpg?t=st=1739885266~exp=1739888866~hmac=6ff8276b323a2b8e5f04824f55bce1b26bdec002655407ea23071b48b17f78ca&w=1380",
+    image: "./database.webp",
   },
   {
     title: "Database Management",
     description: "SQL, MySQL, PostgreSQL, NoSQL, MongoDB",
-    image: "./5357389_2794206.svg",
+    // image: "./5357389_2794206.svg",
+    image: "./test.webp",
   },
   {
     title: "Agile Methodologies",
     description: "Scrum, Kanban, Git, and Azure DevOps",
-    image: "https://img.freepik.com/free-vector/flat-scrum-task-board-with-hands-team-members-color-paper-stickers-group-software-developers-create-work-project-schedule-with-sticky-notes-teamwork-development-sprint-planning-concept_88138-909.jpg?t=st=1739891419~exp=1739895019~hmac=d7a9f5c53c8cc8f9aa6c88459c6cfd87f02001d3b2e76f4cabc905f0dbbceaa0&w=1380",
+    // image: "https://img.freepik.com/free-vector/flat-scrum-task-board-with-hands-team-members-color-paper-stickers-group-software-developers-create-work-project-schedule-with-sticky-notes-teamwork-development-sprint-planning-concept_88138-909.jpg?t=st=1739891419~exp=1739895019~hmac=d7a9f5c53c8cc8f9aa6c88459c6cfd87f02001d3b2e76f4cabc905f0dbbceaa0&w=1380",
+    image: "./test2.webp",
   },
 ];
 
